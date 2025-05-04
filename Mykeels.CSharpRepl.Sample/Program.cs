7e3b24170102cb755474d730f40ae8815886b18b
mykeels <mykehell123@gmail.com>
2025-05-04 12:23:43 -0400

    feat: add Mykeels.CSharpRepl.Sample project to solution

---
+++ Mykeels.CSharpRepl.Sample/Program.cs
@@ -0,0 +1,30 @@
+﻿using CSharpRepl.Services;
+using Mykeels.CSharpRepl;
+using Spectre.Console;
+
+await Repl.Run(
+    new Configuration(
+        references: AppDomain
+            .CurrentDomain.GetAssemblies()
+            .Select(a => $"{a.GetName().Name}.dll")
+            .ToArray(),
+        usings: ["System", "System.Collections.Generic", "System.Linq"],
+        applicationName: "Mykeels.CSharpRepl.Sample",
+        logSuccess: (message, result) => {
+            Console.WriteLine($"<< {message}");
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(
+                result
+            );
+            AnsiConsole.MarkupLine($"[green]>> {output.EscapeMarkup().Trim()}[/]");
+        },
+        logError: (message, exception, _) => {
+            Console.WriteLine($"<< {message}");
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(
+                new {
+                    Error = exception
+                }
+            );
+            AnsiConsole.MarkupLine($"[red]>> {output.EscapeMarkup().Trim()}[/]");
+        }
+    )
+);
